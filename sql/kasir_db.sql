87e031f3e597ce7d810be112f4f351c401ca2b6a
unknown <rayfathur2006@gmail.com>
2024-04-17 23:19:58 +0700

    FEAT: Auth System Ench

--- sql/kasir_db.sql
+++ sql/kasir_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 17, 2024 at 04:24 PM
+-- Generation Time: Apr 17, 2024 at 06:19 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `kasir_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` tinyint(1) UNSIGNED NOT NULL COMMENT '0 inactive, 1 active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `nama`, `username`, `email`, `password`, `created_at`, `status`) VALUES
+(1, 'Rayhan-Chan', 'admin', 'email@example.com', '$2y$10$xVKaOwFif8dOkhkVfnRV1Owhhp/ScYenrPmhmONxL1K9L.W9X6PW.', '2024-04-17 14:32:48', 1);
 
 -- --------------------------------------------------------
 
@@ -71,7 +94,8 @@ CREATE TABLE `member` (
 INSERT INTO `member` (`id`, `nik`, `nama`, `nomor_telepon`, `alamat`, `tanggal_registrasi`, `expired_date`, `status`, `is_deleted`, `deleted_at`) VALUES
 ('3281240308000001', '1234567890123457', 'Muhammad Rayhan Fathurrakhman', '081224018624', 'Sanggar Indah Lestari, blok F4 no 4, RT 6/RW 12, Desa Nagrak, Kecamatan Cangkuang, Kab. Bandung, Jawa Barat, Indonesia', '2024-03-08', '2024-05-16', 1, 0, NULL),
 ('4238240301000002', '1234567890123456', 'Gojo Satoru', '081224018624', 'Jl. Halo halo bandung', '2024-03-01', '2024-04-27', 0, 0, NULL),
-('7563240312000003', '1122334455667789', 'Bika ambon', '0829838136', 'unuynjuyjnuy', '2024-03-12', '2024-06-15', 1, 0, NULL),
+('5545240417000004', '1234567890123400', 'Kajni', '081224018633', 'Jl. Perjalanan tanpa akhir', '2024-04-02', '2024-04-30', 1, 1, '2024-04-16 22:45:05'),
+('7563240312000003', '1122334455667789', 'Bika ambon 2', '0829838136', 'unuynjuyjnuy', '2024-03-12', '2024-07-15', 1, 0, NULL),
 ('9985240416000004', '1234567890123409', 'Gol D. Roger', '081231311213', 'Logue Town', '2024-04-16', '2025-11-01', 1, 1, '2024-04-17 02:25:50');
 
 -- --------------------------------------------------------
@@ -95,8 +119,8 @@ CREATE TABLE `produk` (
 
 INSERT INTO `produk` (`id`, `nama_produk`, `harga`, `stok`, `is_deleted`, `deleted_at`) VALUES
 ('BRG0001', 'Pemutih Baju X 250ml', 15000.00, 90, 0, NULL),
-('BRG0002', 'Gayung Super Saderek', 9000.00, 4, 0, NULL),
-('BRG0003', 'Ikan Girang', 13000.14, 80, 0, NULL),
+('BRG0002', 'Gayung Super Saderek', 9000.00, 15, 0, NULL),
+('BRG0003', 'Ikan Girang', 13000.14, 75, 0, NULL),
 ('BRG0004', 'Boneka Panda', 80000.00, 77, 1, '2024-04-17 01:56:25'),
 ('BRG0005', 'Member Card', 50000.00, 997, 0, NULL),
 ('BRG0006', 'Perpanjang Membership 1 Bulan', 10000.00, 2147482580, 0, NULL);
@@ -110,7 +134,8 @@ INSERT INTO `produk` (`id`, `nama_produk`, `harga`, `stok`, `is_deleted`, `delet
 CREATE TABLE `transaksi` (
   `id` varchar(12) NOT NULL,
   `tanggal_transaksi` date NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `admin_id` int(11) DEFAULT NULL,
   `member_id` varchar(16) DEFAULT NULL,
   `subtotal_harga` decimal(10,2) NOT NULL,
   `diskon_id` int(11) DEFAULT NULL,
@@ -122,17 +147,19 @@ CREATE TABLE `transaksi` (
 -- Dumping data for table `transaksi`
 --
 
-INSERT INTO `transaksi` (`id`, `tanggal_transaksi`, `user_id`, `member_id`, `subtotal_harga`, `diskon_id`, `diskon`, `cash`) VALUES
-('202404100007', '2024-04-10', 1, '3281240308000001', 18000.00, NULL, NULL, 35000.00),
-('202404100014', '2024-04-10', 1, NULL, 95000.28, NULL, NULL, 80000.00),
-('202404110001', '2024-04-11', 9, NULL, 18000.00, NULL, NULL, 20000.00),
-('202404130001', '2024-04-13', 1, '7563240312000003', 27000.00, NULL, NULL, 30000.00),
-('202404130002', '2024-04-13', 1, NULL, 60000.00, NULL, NULL, 60000.00),
-('202404130003', '2024-04-13', 1, '4238240301000002', 75000.00, 1, 25, 60000.00),
-('202404160001', '2024-04-16', 8, '7563240312000003', 470000.00, 1, 25, 360000.00),
-('202404160002', '2024-04-16', 1, '7563240312000003', 10200000.00, 6, 99, 102000.00),
-('202404170001', '2024-04-17', 1, '7563240312000003', 45000.00, 1, 25, 35000.00),
-('202404170002', '2024-04-17', 9, '7563240312000003', 115000.00, 1, 25, 87000.00);
+INSERT INTO `transaksi` (`id`, `tanggal_transaksi`, `user_id`, `admin_id`, `member_id`, `subtotal_harga`, `diskon_id`, `diskon`, `cash`) VALUES
+('202404100007', '2024-04-10', 1, NULL, '3281240308000001', 18000.00, NULL, NULL, 35000.00),
+('202404100014', '2024-04-10', 1, NULL, NULL, 95000.28, NULL, NULL, 80000.00),
+('202404110001', '2024-04-11', 9, NULL, NULL, 18000.00, NULL, NULL, 20000.00),
+('202404130001', '2024-04-13', 1, NULL, '7563240312000003', 27000.00, NULL, NULL, 30000.00),
+('202404130002', '2024-04-13', 1, NULL, NULL, 60000.00, NULL, NULL, 60000.00),
+('202404130003', '2024-04-13', 1, NULL, '4238240301000002', 75000.00, 1, 25, 60000.00),
+('202404160001', '2024-04-16', 8, NULL, '7563240312000003', 470000.00, 1, 25, 360000.00),
+('202404160002', '2024-04-16', 1, NULL, '7563240312000003', 10200000.00, 6, 99, 102000.00),
+('202404170001', '2024-04-17', 1, NULL, '7563240312000003', 45000.00, 1, 25, 35000.00),
+('202404170002', '2024-04-17', 9, NULL, '7563240312000003', 115000.00, 1, 25, 87000.00),
+('202404170003', '2024-04-17', NULL, 1, NULL, 36000.00, NULL, NULL, 40000.00),
+('202404170004', '2024-04-17', 1, NULL, '7563240312000003', 65000.70, 1, 25, 50000.00);
 
 -- --------------------------------------------------------
 
@@ -166,7 +193,9 @@ INSERT INTO `transaksi_detail` (`id`, `transaksi_id`, `produk_id`, `harga_satuan
 (23, '202404160002', 'BRG0006', 10000.00, 1020),
 (24, '202404170001', 'BRG0001', 15000.00, 3),
 (25, '202404170002', 'BRG0005', 50000.00, 2),
-(26, '202404170002', 'BRG0001', 15000.00, 1);
+(26, '202404170002', 'BRG0001', 15000.00, 1),
+(27, '202404170003', 'BRG0002', 9000.00, 4),
+(28, '202404170004', 'BRG0003', 13000.14, 5);
 
 --
 -- Triggers `transaksi_detail`
@@ -190,7 +219,6 @@ CREATE TABLE `user` (
   `username` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` tinyint(1) UNSIGNED NOT NULL COMMENT '"0" for admin, "1" for petugas',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `status` tinyint(1) UNSIGNED NOT NULL COMMENT '0 inactive, 1 active',
   `is_deleted` tinyint(4) NOT NULL DEFAULT 0,
@@ -201,14 +229,22 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `nama`, `username`, `email`, `password`, `role`, `created_at`, `status`, `is_deleted`, `deleted_at`) VALUES
-(1, 'M. Rayhan F', 'administrator', 'rayfathur2006@gmail.com', '$2y$10$SdVi/8EjkGEgiukhf7WvrOa47wfTWL4McQhzHWJgRYo9HxeihEU8m', 0, '2024-03-21 02:16:22', 1, 0, NULL),
-(8, 'petugas', '123456', 'petugas@gmail.com', '$2y$10$foKgN2fd8s5EHkvj1jj7wO2.APYYf8IMKjblmz22OMhbmft9PPjOS', 1, '2024-03-23 13:30:52', 1, 0, NULL),
-(9, 'King Slayer', 'vice_petugas', 'vicepetugas@outlook.com', '$2y$10$4JAVyVKASbnDsQp1EPVZpeQcZPpALBNKKlKPLtQL0Bf.XV8ZpdgRe', 1, '2024-03-23 13:32:11', 1, 0, NULL);
+INSERT INTO `user` (`id`, `nama`, `username`, `email`, `password`, `created_at`, `status`, `is_deleted`, `deleted_at`) VALUES
+(1, 'M. Rayhan F', 'administrator', 'rayfathur2006@gmail.com', '$2y$10$SdVi/8EjkGEgiukhf7WvrOa47wfTWL4McQhzHWJgRYo9HxeihEU8m', '2024-03-21 02:16:22', 1, 0, NULL),
+(8, 'petugas', '123456', 'petugas@gmail.com', '$2y$10$foKgN2fd8s5EHkvj1jj7wO2.APYYf8IMKjblmz22OMhbmft9PPjOS', '2024-03-23 13:30:52', 1, 1, '2024-04-16 22:36:28'),
+(9, 'King Slayer', 'petugas', 'vicepetugas@outlook.com', '$2y$10$4JAVyVKASbnDsQp1EPVZpeQcZPpALBNKKlKPLtQL0Bf.XV8ZpdgRe', '2024-03-23 13:32:11', 1, 0, NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_username` (`username`),
+  ADD UNIQUE KEY `unique_email` (`email`);
 
 --
 -- Indexes for table `diskon`
@@ -236,7 +272,8 @@ ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id`),
   ADD KEY `pelanggan_id` (`member_id`),
   ADD KEY `user_id` (`user_id`),
-  ADD KEY `diskon_id` (`diskon_id`);
+  ADD KEY `diskon_id` (`diskon_id`),
+  ADD KEY `admin_id` (`admin_id`);
 
 --
 -- Indexes for table `transaksi_detail`
@@ -259,6 +296,12 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `diskon`
 --
 ALTER TABLE `diskon`
@@ -268,7 +311,7 @@ ALTER TABLE `diskon`
 -- AUTO_INCREMENT for table `transaksi_detail`
 --
 ALTER TABLE `transaksi_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -286,7 +329,8 @@ ALTER TABLE `user`
 ALTER TABLE `transaksi`
   ADD CONSTRAINT `transaksi_ibfk_4` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   ADD CONSTRAINT `transaksi_ibfk_5` FOREIGN KEY (`diskon_id`) REFERENCES `diskon` (`id`),
-  ADD CONSTRAINT `transaksi_ibfk_6` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`);
+  ADD CONSTRAINT `transaksi_ibfk_6` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`),
+  ADD CONSTRAINT `transaksi_ibfk_7` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`);
 
 --
 -- Constraints for table `transaksi_detail`
